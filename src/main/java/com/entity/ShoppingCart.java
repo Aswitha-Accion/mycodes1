728ba5bd9c0261f53ad9b6fcb583f1f10b45004a
Aswitha-Accion <aswitha.pathireddy@accionlabs.com>
2023-04-10 22:17:57 +0530

    uploading files

--- src/main/java/com/entity/ShoppingCart.java
+++ src/main/java/com/entity/ShoppingCart.java
@@ -5,7 +5,42 @@ public class ShoppingCart {
     public int id;
     public String name;
     public String productname;
-
     public int productquantity;
     public float amount;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public int getProductquantity() {
+        return productquantity;
+    }
+    public void setProductquantity(int productquantity) {
+        this.productquantity = productquantity;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
 }
